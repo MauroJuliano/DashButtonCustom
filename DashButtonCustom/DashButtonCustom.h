1f34064a59686a98ef85c1f401b99f8ceaf0d8e6
MauroJuliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-19 19:34:03 -0400

    Initial Commit

---
+++ DashButtonCustom/DashButtonCustom.h
@@ -0,0 +1,18 @@
+//
+//  DashButtonCustom.h
+//  DashButtonCustom
+//
+//  Created by user195713 on 7/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DashButtonCustom.
+FOUNDATION_EXPORT double DashButtonCustomVersionNumber;
+
+//! Project version string for DashButtonCustom.
+FOUNDATION_EXPORT const unsigned char DashButtonCustomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DashButtonCustom/PublicHeader.h>
+
+
